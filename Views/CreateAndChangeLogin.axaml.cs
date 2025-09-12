bf1551fcbf24ec19f9ad2e36e54000136d175cbc
Арифуллин Саид Аскарович <212302@mck-ktits.ru>
2025-09-12 12:14:40 +0300

    добавление login add, реализация добавления и окна

--- Views/CreateAndChangeLogin.axaml.cs
+++ Views/CreateAndChangeLogin.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using AvaloniaProject1pw.Data;
 
 namespace AvaloniaProject1pw;
 
@@ -9,5 +11,39 @@ public partial class CreateAndChangeLogin : Window
     public CreateAndChangeLogin()
     {
         InitializeComponent();
+    }
+    private void AddButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var username = UsernameTB.Text;
+        var password = PasswordTB.Text;
+        var userIdInput = UserIdTB.Text;
+
+
+
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        {
+            return;
+        }
+
+        using var context = new AppDbContext();
+        var newLogin = new Login
+        {
+            Login1 = username,
+            Password = password
+        };
+
+
+        if (!string.IsNullOrWhiteSpace(userIdInput))
+        {
+
+            if (int.TryParse(userIdInput, out int userId))
+            {
+                newLogin.UserId = userId;
+            }
+        }
+
+        context.Logins.Add(newLogin);
+        context.SaveChanges();
+        this.Close();
     }
 }
